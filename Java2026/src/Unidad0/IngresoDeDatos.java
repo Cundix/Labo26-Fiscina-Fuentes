f2ce48db26df9f5ad2ba20d1b3d1edb1730e77cb
Cundix <facufisci@gmail.com>
2026-03-13 17:08:46 -0300

    1ST JAVA EXERCISE

---
+++ Java2026/src/Unidad0/IngresoDeDatos.java
@@ -0,0 +1,16 @@
+package Unidad0;
+
+public class IngresoDeDatos {
+    public static void main(String[] args) {
+        int N = 1;
+        double A = 20;
+        char C = 'C';
+
+        System.out.println(C);
+        System.out.println(N+A);
+        System.out.println(A-N);
+        int valorNumerico = C - 'A';
+        System.out.println(valorNumerico);
+
+    }
+}
