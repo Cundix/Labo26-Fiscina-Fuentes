866536659eeba939bfe7d8dab77c3de841ecb1fe
Cundix <facufisci@gmail.com>
2026-03-18 09:41:43 -0300

    ReemplazoLetra corregido

--- Java2026/src/Unidad0/ReemplazoLetra.java
+++ Java2026/src/Unidad0/ReemplazoLetra.java
@@ -1,28 +1,30 @@
 package Unidad0;
-import java.lang.String;
 import java.util.Scanner;
 
-public class ReemplazoLetra {
-    public static void main(String[] args) {
+public class ReemplazoLetra
+{
+    public static void main(String[] args)
+    {
         String texto = "Ayer me compre muñecos de la marca ToyCo por internet";
-        texto = texto.toLowerCase();
-        String vocales ="aeiou";
-        int numeroVocales = 0;
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese la vocal de reemplazo: ");
+        char vocalUsuario = scanner.next().charAt(0);
 
-        char vocal = scanner.next().charAt(0);
+        String nuevoTexto = "";
 
-        String nuevoTexto;
         for (int i = 0; i < texto.length(); i++)
         {
-            if (texto.charAt(i) == 'e')
-            {
-                nuevoTexto = nuevoTexto + vocal;
+            char caracterActual = texto.charAt(i);
 
+            if (caracterActual == 'e') {
+                nuevoTexto = nuevoTexto.concat(String.valueOf(vocalUsuario));
             }
-            
+            else
+            {
+                nuevoTexto = nuevoTexto.concat(String.valueOf(caracterActual));
+            }
         }
 
-        System.out.println(numeroVocales);
+        System.out.println("Texto modificado: " + nuevoTexto);
     }
 }
