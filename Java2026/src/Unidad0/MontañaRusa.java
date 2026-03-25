d5f2381e6b9adb543f834f7e55df05d8e4fb07f7
Cundix <facufisci@gmail.com>
2026-03-25 09:54:33 -0300

    ej10 corregido por Juanse B) Cundix un 1

--- Java2026/src/Unidad0/MontañaRusa.java
+++ Java2026/src/Unidad0/MontañaRusa.java
@@ -6,24 +6,26 @@ public class MontañaRusa {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean sigo = true;
-        boolean menorAltura = false;
         int contador = 0;
         float promedio = 0;
         float altura = 0;
         while(sigo)
         {
+            System.out.println("Ingresa altura: ");
             altura = scanner.nextFloat();
             if(altura == 0) sigo = false;
             else
             {
-                promedio += altura;
-                if(altura < 1.40) menorAltura = true;
-                contador++;
+                if(altura < 1.40) System.out.println("Altura menor a la permitida");
+                else
+                {
+                    promedio += altura;
+                    contador++;
+                }
             }
         }
         promedio /= contador;
         System.out.println("El promedio de alturas es: " + promedio);
-        if(menorAltura) System.out.println("ADVERTENCIA: Se encontro una persona de menos de 1.40");
         System.out.println("Subieron " + contador + " personas");
     }
 }
