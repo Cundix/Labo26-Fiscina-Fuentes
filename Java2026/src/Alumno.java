ca0b017f2f3f05ada1fe504370fdb07f0f24f522
Cundix <facufisci@gmail.com>
2026-04-15 09:49:39 -0300

    no terminamos materias ni alumno (ej6)

---
+++ Java2026/src/Alumno.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Alumno {
+    private String nombre;
+    private  String apellido;
+    private Fecha fechaNacimiento;
+    private ArrayList<Float> listaNotas;
+    private ArrayList<Materias> materias;
+
+    public Fecha getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Fecha fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    public ArrayList<Float> getListaNotas() {
+        return listaNotas;
+    }
+    public void setListaNotas(ArrayList<Float> listaNotas) {
+        this.listaNotas = listaNotas;
+    }
+
+    public void addNota(float nota){
+        this.listaNotas.add(nota);
+    }
+
+
+}
